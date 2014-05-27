5e731d16d1b4afa44a96ab54311a31157af165de
Cinder <cinder.roxley@phoenixviewer.com>
2014-05-27 11:16:40 -0600

    add darwin target

--- build-cmd.sh
+++ build-cmd.sh
@@ -19,7 +19,34 @@ set +x
 eval "$("$AUTOBUILD" source_environment)"
 set -x
 
-top="$(pwd)
-stage="$(pwd)/stage"
+top="$(dirname "$0")"
+stage="$(pwd)"
 
+pushd "$top/libidn"
+    case "$AUTOBUILD_PLATFORM" in
+        "windows")
+            echo "Windows is not supported yet"
+            fail
+        ;;
+        "darwin")
+            make clean
+            sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/
+            opts="${TARGET_OPTS:--arch i386 -arch x86_64 -iwithsysroot $sdk -mmacosx-version-min=10.6}" 
+            CC="clang" CFLAGS="$opts" CXXFLAGS="$opts" LDFLAGS="$opts" \
+                ./configure --prefix="$stage"
+            make
+            make install
+
+            mkdir -p "$stage/include/idn"
+            mv "$stage"/include/*.h "$stage/include/idn/"
+        ;;
+        "linux")
+            echo "Linux is not supported yet"
+            fail
+        ;;
+    esac
+
+    mkdir -p "$stage/LICENSES"
+    cp "COPYING.LESSERv2" "$stage/LICENSES/libidn.txt"
+popd
 pass
