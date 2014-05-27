57f29638c20f9839d0fccc3c22c2fde7889095d6
Cinder <cinder.roxley@phoenixviewer.com>
2014-05-27 10:36:20 -0600

    Autobuild bones

---
+++ build-cmd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# turn on verbose debugging output for parabuild logs.
+set -x
+# make errors fatal
+set -e
+
+# Check autobuild is around or fail
+if [ -z "$AUTOBUILD" ] ; then
+    fail
+fi
+
+if [ "$OSTYPE" = "cygwin" ] ; then
+    export AUTOBUILD="$(cygpath -u $AUTOBUILD)"
+fi
+
+# Load autobuild provided shell functions and variables
+set +x
+eval "$("$AUTOBUILD" source_environment)"
+set -x
+
+top="$(pwd)
+stage="$(pwd)/stage"
+
+pass
